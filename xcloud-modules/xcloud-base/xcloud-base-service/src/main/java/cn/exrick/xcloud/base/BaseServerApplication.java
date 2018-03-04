c9367e30cb5e1c9fddd3e74d9a866d61641d4b0a
Exrick <1012139570@qq.com>
2018-03-04 18:22:53 +0800

    [INIT]创建分支

--- xcloud-modules/xcloud-base/xcloud-base-service/src/main/java/cn/exrick/xcloud/base/BaseServerApplication.java
+++ xcloud-modules/xcloud-base/xcloud-base-service/src/main/java/cn/exrick/xcloud/base/BaseServerApplication.java
@@ -1,9 +1,9 @@
 package cn.exrick.xcloud.base;
 
-import cn.exrick.xcloud.common.config.RedisCachingConfig;
 import cn.exrick.xcloud.common.config.Swagger2Config;
 import cn.exrick.xcloud.common.exception.RestCtrlExceptionHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -15,9 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringCloudApplication
 //可按需引入全局异常拦截、Swagger2、Redis等配置类[当然推荐配置扫描包]
 @ComponentScan({"cn.exrick.xcloud.base", "cn.exrick.xcloud.common"})
-//@Import({RestCtrlExceptionHandler.class, Swagger2Config.class, RedisCachingConfig.class})
+//@Import({RestCtrlExceptionHandler.class, Swagger2Config.class})
 //启用JPA审计
 @EnableJpaAuditing
+@EnableCaching
 public class BaseServerApplication {
 
     public static void main(String[] args) {
