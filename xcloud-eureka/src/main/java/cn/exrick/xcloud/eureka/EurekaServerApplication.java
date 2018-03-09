19377e93ac670898769ebb76d4189dd9221fa3a8
Exrick <1012139570@qq.com>
2018-03-09 10:10:21 +0800

    [UPDATE]按官方更新新依赖

--- xcloud-eureka/src/main/java/cn/exrick/xcloud/eureka/EurekaServerApplication.java
+++ xcloud-eureka/src/main/java/cn/exrick/xcloud/eureka/EurekaServerApplication.java
@@ -2,7 +2,6 @@ package cn.exrick.xcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
